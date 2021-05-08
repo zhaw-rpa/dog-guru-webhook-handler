b50b254cdf2830433b4ad36eed29a031f913a882
Björn Scheppler <scep@zhaw.ch>
2021-05-08 13:50:41 +0200

    Make ready for deployment to Azure App Service

--- src/main/java/ch/zhaw/rpa/dogguruwebhookhandler/DogGuruWebhookHandlerApplication.java
+++ src/main/java/ch/zhaw/rpa/dogguruwebhookhandler/DogGuruWebhookHandlerApplication.java
@@ -2,7 +2,6 @@ package ch.zhaw.rpa.dogguruwebhookhandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hauptklasse für die RestService-Template-SpringBoot-Applikation
@@ -10,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author scep
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DogGuruWebhookHandlerApplication {
     public static void main(String[] args){
         SpringApplication.run(DogGuruWebhookHandlerApplication.class, args);
